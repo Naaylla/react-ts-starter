5899e4c552fa3e47f6558540d90db97286c72d97
Naaylla <96737235+Naaylla@users.noreply.github.com>
2025-07-17 00:34:29 +0100

    added layout and router

---
+++ src/router.tsx
@@ -0,0 +1,19 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import Layout from "./components/layout"
+
+const router = createBrowserRouter([
+//   { path: "/login", element: <Login /> },
+//   { path: "/register", element: <Register /> },
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { path: "", element: <Home /> },
+    ],
+  },
+]);
+
+export default function Routes() {
+  return <RouterProvider router={router} />;
+}
