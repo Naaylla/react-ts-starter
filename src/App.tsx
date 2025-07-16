dbab6afb20f6b1b73637a6de6cc46d1a1257b0bb
Naaylla <96737235+Naaylla@users.noreply.github.com>
2025-07-17 00:38:15 +0100

    prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
-  return (
-    <>
-      hello world
-    </>
-  )
+  return <>hello world</>
 }
 
 export default App
--- src/router.tsx
+++ src/router.tsx
@@ -1,19 +1,17 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from "./pages/Home";
-import Layout from "./components/layout"
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './pages/Home'
+import Layout from './components/layout'
 
 const router = createBrowserRouter([
-//   { path: "/login", element: <Login /> },
-//   { path: "/register", element: <Register /> },
+  //   { path: "/login", element: <Login /> },
+  //   { path: "/register", element: <Register /> },
   {
-    path: "/",
+    path: '/',
     element: <Layout />,
-    children: [
-      { path: "", element: <Home /> },
-    ],
+    children: [{ path: '', element: <Home /> }],
   },
-]);
+])
 
 export default function Routes() {
-  return <RouterProvider router={router} />;
+  return <RouterProvider router={router} />
 }
